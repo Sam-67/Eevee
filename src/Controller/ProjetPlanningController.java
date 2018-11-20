54d54e22040676da928304a9ef54c07103c4dedb
smesbah <sarah.mesbah@etu.unistra.fr>
2018-11-20 11:49:04 +0100

    adding of comparing functions

--- src/Controller/ProjetPlanningController.java
+++ src/Controller/ProjetPlanningController.java
@@ -10,6 +10,20 @@ public class ProjetPlanningController {
         return instance;
     }
     
+    public void getProjetReport() {
+    	// 1. Calculate number of days between two deadlines
+    	
+    	// 2. Calculate number of developpment days per developpers 
+    	
+    	// 3. Compare the deadlines days and the workload days
+    	
+    	// 4. If the deadlines days are superior than workload days : PROJECT IS OK  
+    	
+    	
+    	// 5. If the workload days are superior then deadlines days : ADD ANOTHER DEV OR PROJECT MANAGER
+    	
+    }
+    
     public int getNbDaysbetween() {
     	return 0; 
     }
@@ -20,6 +34,18 @@ public class ProjetPlanningController {
     
     public int getNbProjetManagement(int nbProjectManagementDays, int nbProjectManager) {
     	return (nbProjectManagementDays/nbProjectManager);
+    }
+    
+    public int compare(int deadlines_date, int workload_date) {
+    	if(deadlines_date > workload_date) {
+    		return 1;
+    	} else {
+    		return 0;
+    	}
+    }
+    
+    public void compareProjects() {
+    	
     }
     
     
